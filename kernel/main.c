2b25ad47db474d5b8186e84ac6aa2a5bc6d7fc32
LiuziqiOvO <1021578619@qq.com>
2023-05-31 03:36:28 +0800

    start_hart, new sbi call

--- kernel/main.c
+++ kernel/main.c
@@ -61,6 +61,9 @@ main(unsigned long hartid, unsigned long dtb_pa)
     for(int i = 1; i < NCPU; i++) {
       unsigned long mask = 1 << i;
       sbi_send_ipi(&mask);
+      // lzq
+      extern char _entry[];
+      start_hart(i, (uint64)_entry, 0);
     }
     __sync_synchronize();
     started = 1;
